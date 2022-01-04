e15dcb492c94af0e8285a35e61c6cabf49647988
jc <kczarnecok@wp.pl>
2022-01-04 09:16:14 +0100

    Add authorization for update and delete methods

--- backend/Routers/Tournament/middlewares.js
+++ backend/Routers/Tournament/middlewares.js
@@ -5,9 +5,10 @@ const authorizeOrginizer = async (req, res, next) => {
     res.tournament = await Tournament.findById(req.params.id);
 
     if (res._id.id != res.tournament.organizerId) {
-      res.status(401).send("You must be orginizer to edit the tournament.");
+      res.status(401).send("You must be orginizer of the tournament.");
       return;
     }
+
     next();
   } catch (err) {
     res.status(500).json(err);
--- backend/Routers/Tournament/tournamentRouter.js
+++ backend/Routers/Tournament/tournamentRouter.js
@@ -26,50 +26,50 @@ tournamentRouter.post("/create", async (req, res, next) => {
 
 //open tournament by id and
 //generate empty bracket for tournament with specified bracketSize
-tournamentRouter.put(
-  "/:id/init",
-  authorizeOrginizer,
-  async (req, res, next) => {
-    try {
-      const arr = [];
-
-      if (res.tournament.matches) {
-        res.status(400).json("Tournament has been already initialized.");
-        return;
-      }
-
-      for (let i = 1; i < res.tournament.bracketSize; i++) {
-        arr.push({
-          number: i,
-          participants: [],
-          winnerID: null,
-        });
-      }
-
-      await Tournament.findOneAndUpdate(
-        { _id: req.params.id },
-        {
-          $set: { matches: arr },
-        },
-        { useFindAndModify: false }
-      );
-
-      console.log("c");
-
-      res.status(200).json("Tournament has been initialized succesfully.");
-    } catch (err) {
-      next(err);
-    }
-  }
-);
-
-//update tournament by id
-tournamentRouter.put("/:id", async (req, res, next) => {
+tournamentRouter.put("/:id/init", authorizeOrginizer, async (req, res, next) => {
   try {
+    const arr = [];
+
+    if (res.tournament.matches) {
+      res.status(400).json("Tournament has been already initialized.");
+      return;
+    }
+
+    for (let i = 1; i < res.tournament.bracketSize; i++) {
+      arr.push({
+        number: i,
+        participants: [],
+        winnerID: null,
+      });
+    }
+
     await Tournament.findOneAndUpdate(
       { _id: req.params.id },
       {
-        $set: req.body,
+        $set: { matches: arr },
+      },
+      { useFindAndModify: false }
+    );
+
+    res.status(200).json("Tournament has been initialized succesfully.");
+  } catch (err) {
+    next(err);
+  }
+});
+
+//update tournament by id
+tournamentRouter.put("/:id", authorizeOrginizer, async (req, res, next) => {
+  try {
+    const updatedTournament = {};
+    req.body.tournamentName && (updatedTournament.tournamentName = req.body.tournamentName);
+    req.body.date && (updatedTournament.date = req.body.date);
+    req.body.location && (updatedTournament.location = req.body.location);
+    req.body.description && (updatedTournament.description = req.body.description);
+
+    await Tournament.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: updatedTournament,
       },
       { useFindAndModify: false }
     );
@@ -100,7 +100,7 @@ tournamentRouter.get("/:id", async (req, res, next) => {
 });
 
 //delete tournament by id
-tournamentRouter.delete("/:id", async (req, res, next) => {
+tournamentRouter.delete("/:id", authorizeOrginizer, async (req, res, next) => {
   try {
     await Tournament.findOneAndDelete({ _id: req.params.id });
     res.status(200).send(`Tournament has been deleted.`);

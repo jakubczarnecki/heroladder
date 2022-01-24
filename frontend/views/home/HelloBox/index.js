4688f4e2dc27b1c7dd9e7647b58d629b6977ac3b
229935 <229935@edu.p.lodz.pl>
2022-01-24 01:40:01 +0100

    make the motivational sentences on main page random and fun

--- frontend/views/home/HelloBox/index.js
+++ frontend/views/home/HelloBox/index.js
@@ -1,20 +1,25 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { HelloWrapper, TextWrapper, AvatarWrapper } from "./styled"
 import { Title, Paragraph } from "../../../components/Layout"
 import { Avatar } from "../../../components/misc"
+import { sentences } from "./sentences"
 
 const HelloBox = () => {
     const user = useSelector((state) => state.user)
+
+    const [rnd, setRnd] = useState(null)
+
+    useEffect(() => {
+        setRnd(Math.floor(Math.random() * sentences.length))
+        console.log(rnd)
+    }, [])
 
     return (
         <HelloWrapper>
             <TextWrapper>
                 <Title>Hello {user.username}!</Title>
-                <Paragraph>
-                    Słuchaczy o mocnych nerwach zapraszmy na porcję ostrego,
-                    metalowego łojenia, przed państwem zespół ,
-                </Paragraph>
+                <Paragraph>{rnd ? sentences[rnd] : null}</Paragraph>
             </TextWrapper>
             <AvatarWrapper>
                 <Avatar size={70} img={user.avatar} />
---
+++ frontend/views/home/HelloBox/sentences.js
@@ -0,0 +1,15 @@
+export const sentences = [
+    "“The elevator to success is out of order. You'll have to use the stairs, one step at a time.” — Joe Girard",
+    "“I am so clever that sometimes I don’t understand a single word of what I am saying.” — Oscar Wilde",
+    "“Work until your bank account looks like a phone number.” — Unknown ",
+    "“Life is like a sewer… what you get out of it depends on what you put into it.” — Tom Lehrer",
+    "“I always wanted to be somebody, but now I realise I should have been more specific.” — Lily Tomlin",
+    "“Just one small positive thought in the morning can change your whole day.” — Dalai Lama",
+    "“Opportunities don't happen, you create them.” — Chris Grosser",
+    "“Love your family, work super hard, live your passion.” — Gary Vaynerchuk",
+    "“It is never too late to be what you might have been.” — George Eliot",
+    "“Don't let someone else's opinion of you become your reality” — Les Brown",
+    "“Can we pretend that airplanes in the night sky are like shootin' stars?” — Hayley N. Williams",
+    "“Hey now, you're an all-star, get your game on, go play” — Steven Harwell",
+    "“We know the game and we're gonna play it” — Rick Astley",
+]
